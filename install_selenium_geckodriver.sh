3d94167dadc61d559a8dcfc9b7ce46e676cc9905
Axon <firstlast123445@yahoo.com>
2023-02-20 01:06:51 +0530

    removed checking of python sel

--- install_selenium_geckodriver.sh
+++ install_selenium_geckodriver.sh
@@ -14,11 +14,11 @@ curl -s -L "$url" | tar -xz
 chmod +x geckodriver
 mv geckodriver "$INSTALL_DIR"
 echo "installed geckodriver binary in $INSTALL_DIR"
-python3 -c "from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
-driver.get(\"http://google.com/\")
-print (\"Headless Firefox Initialized\")
-driver.quit()"
+# python3 -c "from selenium import webdriver
+# from selenium.webdriver.firefox.options import Options
+# options = Options()
+# options.headless = True
+# driver = webdriver.Firefox(options=options)
+# driver.get(\"http://google.com/\")
+# print (\"Headless Firefox Initialized\")
+# driver.quit()"
